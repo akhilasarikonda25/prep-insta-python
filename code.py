692357c0ae1448eb4490716d11aa6620013fac15
lekhanasarikonda <134198650+lekhanasarikonda@users.noreply.github.com>
2023-06-10 12:17:33 +0530

    Update code.py

--- code.py
+++ code.py
@@ -9,4 +9,14 @@ INSTALLATION AND SETUP:
   6) Click on close
   7) Python is installed in our system
   
->>Installing IntelliJ idea for windows based system
+>>Installing IntelliJ idea for windows based system:(Code Editor)
+  
+  1) Go to jetbrains.com
+  2) Go to Toos
+  3) Click on IntelliJIDEA
+  4) Click on download
+  5) Ultimate edition is for bigger companies so we are going to download community edition click on download.
+  6) Click on dowloaded one and the exe file will run click on next if your system is 64 bit tick the checkbox 
+  7) Tick all the associations like java and etc and install
+  8) click on run IntelliJIDEA community edition and click on finish and confirm >> dont send >> light or dark modes
+  
