4385447b32f8328ed006456422dbe3e53d843d7c
lekhanasarikonda <134198650+lekhanasarikonda@users.noreply.github.com>
2023-06-10 11:32:15 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
-print("helloworld")
+INSTALLATION AND SETUP:
+>>Installing python in windows based system Audio:
+  
+  1) Go to python.org website
+  2) Click on Downloads
+  3) Click on windows
+  4) We may see lot of versions of python to install but make sure that you always install latest version of python
+  5) Choose any one installer and click on install now
+  6) Click on close
+  7) Python is installed in our system
+  
+>>Installing IntelliJ idea for windows based system
